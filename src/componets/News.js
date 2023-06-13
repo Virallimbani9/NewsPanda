fa9b7f284ad6df2e7d3675745b66f1c39d120a2f
VIRAL PATEL <viral9411@gmail.com>
2023-06-13 16:47:40 +0530

    a

--- src/componets/News.js
+++ src/componets/News.js
@@ -1,65 +1,64 @@
 import React, { Component } from 'react'
 import Newsiteam from './Newsiteam'
 
-export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ];
-
+export class News extends Component { 
     constructor(){
         super();
         this.state={
-            articles : this.articles,
+            articles : [],
             loading : false,
+            page:1
         }
       }
 
       async componentDidMount(){
         // console.log("cmf")
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=29a776b522a44e2e88983db04b2cb225";
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=72db924db7f2420c8433c975adaa8297&page=1&pageSize=20`";
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({articles : parsedData.articles })
+    
+        this.setState({articles : parsedData.articles ,totalResults:parsedData.totalResults })
+      }
+
+        handleNext = async ()=>{
+            if(this.state.page + 1>Math.ceil(this.state.totalResults/20)){
+
+            }else{
+            let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=72db924db7f2420c8433c975adaa8297&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+            page : this.state.page + 1,
+            articles : parsedData.articles 
+            })}
       }
       
+       handlePrev = async()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=72db924db7f2420c8433c975adaa8297&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+        page : this.state.page - 1,
+        articles : parsedData.articles 
+        })
+      }
+
 render() {
-    console.log("render")
+    // console.log("render")
     return (
     <div className='container my-3'>
         <h2>NewsPanda - Top Headlines</h2>
         
         <div className="row">
-        {this.articles.map((element)=>{
+        {this.state.articles.map((element)=>{
          return <div className="col-md-4" key={element.url} >
             <Newsiteam title={element.title?element.title.slice(0,40):""}  desription={element.description?element.description.slice(0,88):""} imgurl={element.urlToImage} newsurl={element.url}/>
          </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button type="button" disabled={this.state.page<=1} class="btn btn-dark" onClick={this.handlePrev}>&larr; Pervios</button>
+        <button type="button" class="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
         </div>
     </div>
     )
--- src/componets/Newsiteam.js
+++ src/componets/Newsiteam.js
@@ -7,11 +7,11 @@ export class Newsiteam extends Component {
     return (
       <div className='my-3'>
         <div className="card" style= {{width: "18rem"}}>
-            <img src={imgurl} className="card-img-top" alt="..."/>
+            <img src={!imgurl?"https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_1280.jpg":imgurl} className="card-img-top" alt="..."/>
              <div className="card-body">
                   <h5 className="card-title">{title}...</h5>
                   <p className="card-text">{desription}...</p>
-                  <a href={newsurl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                  <a rel='noreferrer' href={newsurl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
       </div>
